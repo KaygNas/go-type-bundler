04517724302e3f49868eb70c2b3ff69f9f4a19e7
huangchengjie <597857074@qq.com>
2024-08-23 01:04:44 +0800

    feat: bundle multiple packages deep

--- internal/pkg/bundler/generator.go
+++ internal/pkg/bundler/generator.go
@@ -30,24 +30,7 @@ func (g *GeneratorImpl) Generate(pkg *packages.Package, cs types.ConflictResolve
 
 	g.writePackageClause(pkg, &s)
 
-	for _, astFile := range pkg.Syntax {
-		ast.Inspect(astFile, func(n ast.Node) (gonext bool) {
-			decl, isGenDecl := n.(*ast.GenDecl)
-
-			if !isGenDecl || decl.Tok != token.TYPE {
-				gonext = true
-				return
-			}
-
-			var buf bytes.Buffer
-			printer.Fprint(&buf, pkg.Fset, decl)
-			s.Write(buf.Bytes())
-			s.Write([]byte("\n\n"))
-
-			gonext = false
-			return
-		})
-	}
+	g.writePkgTypes(pkg, &s, cs)
 
 	formated, formatErr := g.formatCode(s.String())
 	if formatErr != nil {
@@ -60,17 +43,6 @@ func (g *GeneratorImpl) Generate(pkg *packages.Package, cs types.ConflictResolve
 	return
 }
 
-func (*GeneratorImpl) formatCode(rawCode string) (string, error) {
-	utils.Debug("Formating Raw code: \n%s", rawCode)
-
-	formated, formatErr := format.Source([]byte(rawCode))
-	if formatErr != nil {
-		return "", formatErr
-	}
-
-	return string(formated), nil
-}
-
 func (g *GeneratorImpl) writePackageClause(pkg *packages.Package, s *strings.Builder) {
 	if len(pkg.Syntax) > 0 {
 		firstFile := pkg.Syntax[0]
@@ -78,4 +50,74 @@ func (g *GeneratorImpl) writePackageClause(pkg *packages.Package, s *strings.Bui
 		s.WriteString(firstFile.Name.Name)
 		s.WriteString("\n\n")
 	}
+}
+
+func (g *GeneratorImpl) writePkgTypes(pkg *packages.Package, s *strings.Builder, cs types.ConflictResolver) {
+	//TODO: rename types which import external types
+	for _, pkg := range pkg.Imports {
+		g.writePkgTypes(pkg, s, cs)
+	}
+
+	for _, astFile := range pkg.Syntax {
+		ast.Inspect(astFile, func(n ast.Node) (gonext bool) {
+			decl, isGenDecl := n.(*ast.GenDecl)
+
+			if !isGenDecl || decl.Tok != token.TYPE {
+				gonext = true
+				return
+			}
+
+			g.writeTypeDecl(decl, s, cs)
+
+			gonext = false
+			return
+		})
+	}
+}
+
+func (g *GeneratorImpl) writeTypeDecl(typeDecl *ast.GenDecl, s *strings.Builder, cs types.ConflictResolver) {
+	for _, spec := range typeDecl.Specs {
+		typeSpec, isTypeSpec := spec.(*ast.TypeSpec)
+		if !isTypeSpec {
+			continue
+		}
+
+		structType, isStructType := typeSpec.Type.(*ast.StructType)
+		if !isStructType {
+			continue
+		}
+
+		removePackageSelector(structType)
+	}
+
+	var buf bytes.Buffer
+	printer.Fprint(&buf, token.NewFileSet(), typeDecl)
+	s.Write(buf.Bytes())
+	s.Write([]byte("\n\n"))
+}
+
+// remove the package selector
+func removePackageSelector(ts *ast.StructType) {
+	for _, field := range ts.Fields.List {
+		structType, isStructType := field.Type.(*ast.StructType)
+		if isStructType {
+			removePackageSelector(structType)
+		} else {
+			selector, isSelector := field.Type.(*ast.SelectorExpr)
+			if isSelector {
+				field.Type = selector.Sel
+			}
+		}
+	}
+}
+
+func (*GeneratorImpl) formatCode(rawCode string) (string, error) {
+	formated, formatErr := format.Source([]byte(rawCode))
+	if formatErr != nil {
+		return "", formatErr
+	}
+
+	utils.Debug("Raw code formated: \n%s", formated)
+
+	return string(formated), nil
 }
